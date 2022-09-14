a3b90ae949f1f33252302911ffc8627637ed99a4
Kaliandra de Freitas Pereira <kpereira@PSG.LOCAL>
2022-09-14 12:32:30 -0400

    conexão com banco de dados

--- src/main/java/com/kaliandra/curso/entities/User.java
+++ src/main/java/com/kaliandra/curso/entities/User.java
@@ -1,20 +1,29 @@
 package com.kaliandra.curso.entities;
 
 import java.io.Serializable;
-import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
-	
+
 	public User() {
-		
+
 	}
 
 	public User(long id, String name, String email, String phone, String password) {
@@ -70,9 +79,7 @@ public class User implements Serializable {
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
+		if ((obj == null) || (getClass() != obj.getClass()))
 			return false;
 		User other = (User) obj;
 		return id == other.id;
